47db09759ef39ee2decfeac6a11faa31ff2c2200
Ro-svg-7 <aarohisharma0522@gmail.com>
2026-05-14 17:28:41 +0530

    Progress bar added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
 
+timer = None
+progress = 0
+bar_width = 0
+
 window = Tk()
 window.title("Dangerous Writing App")
 
@@ -19,5 +23,43 @@ text_area = Text(writing_frame, bg="grey", fg="white",
                   wrap=WORD, bd=0, padx=30, pady=30)
 
 text_area.pack(fill="both", expand=True)
+
+def update_bar():
+    global bar_width
+
+    if bar_width < 900 :
+        bar_width += 9
+        timer_canvas.coords(progress_bar, 0, 0, bar_width, 8)
+
+        window.after(50, update_bar)
+
+def delete_text():
+    text_area.delete("1.0", END)
+    messagebox.showinfo("Time's Up!", "Your time is up! All your progress has been lost.")
+
+def start_timer(event):
+    global timer
+    if timer is not None:
+        window.after_cancel(timer)
+
+    print("Timer Reset!")
+    global bar_width
+    bar_width = 0
+    timer_canvas.coords(progress_bar, 0,0,0,8)
+    update_bar()
+    timer = window.after(5000, delete_text)
+
+#-----------------------------TIMER-----------------------------#
+timer_canvas = Canvas(window, 
+                      width=900, 
+                      height=8,
+                      bg="#222222",
+                      highlightthickness=0)
+timer_canvas.place(relx=0.5, rely=0.08, anchor="center")
+
+progress_bar = timer_canvas.create_rectangle(
+    0, 0, 0, 8, fill="red", width=0 
+)
+text_area.bind("<KeyPress>", start_timer)
 
 window.mainloop()
